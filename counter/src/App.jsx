c5bbbd4dc674408ede841d5c838961a12dd552d0
learnrahul29 <learnwithrahulyadav1@gmail.com>
2025-01-06 22:38:58 +0530

    done

---
+++ counter/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1>chai aur React</h1>
+      <h2>Counter value:5</h2>
+      <button>add value</button>
+      
+      
+    </>
+  )
+}
+
+export default App
