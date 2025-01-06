996e7c3004ef4f7fe0127fd54931b6530e622350
learnrahul29 <learnwithrahulyadav1@gmail.com>
2025-01-07 00:12:55 +0530

    changed

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -4,14 +4,27 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  let counter = 15
+
+  const addValue = () => {
+    counter = counter + 1
+  }
+
+
 
   return (
     <>
       
       <h1>chai aur React</h1>
-      <h2>Counter value:5</h2>
-      <button>add value</button>
       
+      <h2>Counter value:{counter}</h2>
+
+
+      <button>
+        onClick={addValue}
+        Add value</button>
+        <br />
+      <button>remove value</button>
       
     </>
   )
